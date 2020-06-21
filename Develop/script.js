a91505c272d3d071a66c4e11ca28298551fb6452
jeoldmixon <jeoldmixon@yahoo.com>
2020-06-21 13:49:12 -0500

    final

--- Develop/script.js
+++ Develop/script.js
@@ -1,27 +1,71 @@
-var displayDay = moment().format("dddd,MMMM Do");
+$(function() {
+    console.log("ready!");
 
-function setDate() {
-    $("#displayDay").text(displayDay);
+    //load previously saved items
 
-}
-setDate();
+    var PreviousTasks = [];
+    var loadPreviousTasks = function() {
+        $("#hour-9").find("textarea").val(localStorage.getItem("hour-9"))
+        $("#hour-10").find("textarea").val(localStorage.getItem("hour-10"))
+        $("#hour-11").find("textarea").val(localStorage.getItem("hour-11"))
+        $("#hour-12").find("textarea").val(localStorage.getItem("hour-12"))
+        $("#hour-13").find("textarea").val(localStorage.getItem("hour-13"))
+        $("#hour-14").find("textarea").val(localStorage.getItem("hour-14"))
+        $("#hour-15").find("textarea").val(localStorage.getItem("hour-15"))
+        $("#hour-16").find("textarea").val(localStorage.getItem("hour-16"))
+        $("#hour-17").find("textarea").val(localStorage.getItem("hour-17"))
 
-// var value = $(this)
-//     .siblings('.description')
-//     .val();
 
-// for (let i = 9; i <= 17; i++) {
-//     $
-// }
+    }
 
-// for loop
-// if time is less than current apply past CSS
-//else if time is === to current  (remove past)
-//add current
-//else  add future
-function compareTime()
+    var displayDay = moment().format("dddd, MMMM Do, YYYY");
+    var currentHour = new Date().getHours();
 
-var currentHour = moment().hour(); // Number
-console.log(currentHour);
-$("").addClass("past");
-//if hour < current then apply .past
+    function setDate() {
+        $("#displayDay").text(displayDay);
+
+    }
+    loadPreviousTasks();
+    setDate();
+
+    //save button
+    $(".saveBtn").on("click", function() {
+        var saveButton = $(this)[0];
+        console.log(saveButton);
+
+        var value = $(this)
+            .siblings('textarea')
+            .val();
+
+        var time = $(this)
+            .parent()
+            .attr("id");
+
+        localStorage.setItem(time, value)
+    });
+
+
+    var updateColor = function() {
+        $('.time-block').each(function(index, hour) {
+            var lineHour = parseInt(hour.id.split('-')[1])
+            console.log(lineHour)
+            if (lineHour < currentHour) {
+                $(hour).addClass('past')
+            } else if (lineHour === currentHour) {
+                $(hour).removeClass('past')
+                $(hour).addClass('present')
+            } else {
+                $(hour).removeClass('past')
+                $(hour).removeClass('present')
+                $(hour).addClass('future')
+            }
+        })
+    }
+    updateColor();
+
+
+    // left to do
+    //1- set local storage items and save with click
+    //2-color coded for loop for past present future
+
+});
--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 
 Create a simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
 
-You'll need to use the [Moment.js](https://momentjs.com/) library to work with date and time. Be sure to read the documentation carefully and concentrate on using Moment.js in the browser.
-
 ## User Story
 
 ```
@@ -34,13 +32,5 @@ The following animation demonstrates the application functionality:
 
 ![day planner demo](./Assets/05-third-party-apis-homework-demo.gif)
 
-## Review
 
-You are required to submit the following for review:
 
-* The URL of the deployed application.
-
-* The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
-
-- - -
-© 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
